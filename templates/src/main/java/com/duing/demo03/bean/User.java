bba87f4434248b6dec8bda7d62b8f18ad054e43d
IAGOGWWX <1347395718@qq.com>
2023-07-26 17:05:59 +0800

    thymeleaf语法的拓展知识点

--- templates/src/main/java/com/duing/demo03/bean/User.java
+++ templates/src/main/java/com/duing/demo03/bean/User.java
@@ -1,8 +1,10 @@
 package com.duing.demo03.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class User {
     private int uid;
     private String uname;
